39c3108645099e10981c5d01a5587a03578b8842
Samir <sobiroff23@gmail.com>
2023-11-23 17:35:32 +0500

    fixed

--- modules/main.js
+++ modules/main.js
@@ -1,7 +1,12 @@
-import { headerCreate} from "./ui";
+import { getData } from "./helpers";
+import { headerCreate, reload } from "./ui";
 
 let header = document.querySelector('header')
-
+let place = document.querySelector('.movies')
 headerCreate(header)
 
-
+getData("/movie/now_playing")
+    .then((res) => {
+        console.log(res.data.results);
+        reload(res.data.results, place);
+    });
