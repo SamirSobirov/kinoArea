a67cde3bc8efaed75bad4f503ca96d716cb8c6d1
Samir <sobiroff23@gmail.com>
2023-11-23 18:38:57 +0500

    changed

--- modules/main.js
+++ modules/main.js
@@ -1,11 +1,4 @@
-import { getData } from "./helpers";
-import { headerCreate, reload } from "./ui";
+import { headerCreate } from "./ui";
 
 let header = document.querySelector('header')
-let place = document.querySelector('.movies')
 headerCreate(header)
-
-getData("/movie/now_playing")
-    .then((res) => {
-        reload(res.data.results, place);
-    });
