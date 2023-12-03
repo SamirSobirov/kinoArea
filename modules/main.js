c128f7d15268499753df14bafaa05b36d8a9cc12
Samir <sobiroff23@gmail.com>
2023-12-03 19:34:52 +0500

    fixed

--- modules/main.js
+++ modules/main.js
@@ -132,6 +132,24 @@ search_input.onkeyup = () => {
 btn_close.onclick = () => {
     search_box.classList.remove('visible');
     document.body.style.overflow = 'auto';
-    
+
 };
 
+
+
+
+
+let genre_list = document.querySelector('.genres ul')
+
+getData('/genre/movie/list')
+    .then((genres_res) => {
+        const {
+            data: {
+                genres
+            }
+        } = genres_res
+
+        reload_genres(genres, genre_list)
+    })
+
+    console.log(genre_list);
