c9aa71f43fcf110665823e7539ff34378ba5963e
Samir <sobiroff23@gmail.com>
2023-12-07 13:51:35 +0500

    fixed

--- modules/movieid.js
+++ modules/movieid.js
@@ -20,7 +20,6 @@ getData(`/movie/${movie_id}`)
         let sub_title = document.querySelector('.first-section__sub-title')
         let desrc = document.querySelector('.first-section__txt')
         let rating = document.querySelector('.first-section__header-bottom-rating')
-        let fav = document.querySelector('.first-section__header-bottom-txt')
         let rating_view = data.vote_average.toString().replaceAll('.', '').slice(0, 2)
         let year = document.querySelector('#year')
         let country = document.querySelector('#country')
@@ -36,15 +35,13 @@ getData(`/movie/${movie_id}`)
         let profit = document.querySelector('#profit')
         let premiere_world = document.querySelector('#premiere_world')
         let premiere_RF = document.querySelector('#premiere_RF')
-        let age_limit = document.querySelector('#age_limit')
-        let duration = document.querySelector('#duration')
+  
 
         loc.innerHTML = data.title
         title.innerHTML = data.original_title
         sub_title.innerHTML = data.original_title
         desrc.innerHTML = data.overview
-        rating.firstElementChild.innerHTML = `Рейтинг ожиданий ${rating_view}%`
-        fav.innerHTML = `В избранном у ${Math.round(data.popularity)} человек`
+
         year.innerHTML = data.release_date.split('-').at(0)
         country.innerHTML = ''
         tagline.innerHTML = data.title.split('.').at(0)
@@ -53,12 +50,7 @@ getData(`/movie/${movie_id}`)
         profit.innerHTML = data.revenue + '$'
         premiere_world.innerHTML = data.release_date
         premiere_RF.innerHTML = data.release_date
-        if (data.adult === false) {
-            age_limit.innerHTML = '16+'
-        } else {
-            age_limit.innerHTML = '18+'
-        }
-        duration.innerHTML = `${data.runtime} мин.`
+ 
 
         getData(`/movie/${movie_id}/credits`)
             .then(({ data }) => {
